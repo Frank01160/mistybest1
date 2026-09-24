f6bbb89fe3544f2af314e930bd2eb629822c4d79
neoarforex <franciskinungi211@gmail.com>
2026-09-24 13:08:12 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -29,6 +29,6 @@ db.enablePersistence({ synchronizeTabs: true }).catch((err) => {
 
 /* ===== Fixed account emails ============================================= */
 const FIXED_EMAILS = {
-  seller: "seller@mistycode.local",
-  manager: "manager@mistycode.local",
+  seller: "seller@gmail.com",
+  manager: "manager@gmail.com",
 };
